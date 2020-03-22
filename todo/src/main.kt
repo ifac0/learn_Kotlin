896da74115ee6eade08caedbc609864d4561dbeb
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-22 11:23:56 -0300

    fix: :construction: Estrutura de repetição;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- todo/src/main.kt
+++ todo/src/main.kt
@@ -1,6 +1,39 @@
 fun main(){
-    println("HelloWorld")
 
-    val nome =  "Ivan"
-    println("nome: $nome")
+    for (i in 1..5) {
+
+        /**
+        if(i == 2) {
+            break
+        }
+        **/
+
+        val titular: String =  "Ivan - $i"
+        val numConta: Int = 1000 + 1
+        var saldo: Double = i * 0.134 + 13.5
+
+        println("----------------")
+
+        println("titular: $titular")
+        println("conta: $numConta")
+        println("saldo: $saldo")
+
+        println("----------------")
+
+        statusConta(saldo)
+
+        for (i in 3 downTo 1){
+            println(i)
+        }
+        println()
+    }
+
+}
+
+fun statusConta( saldo: Double) {
+    when {
+        saldo > 0.0 -> println("status: positiva")
+        saldo == 0.0 -> println("status: neutro")
+        else -> println("status: negativa")
+    }
 }
